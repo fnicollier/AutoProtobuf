c642205d8e7af127368510db7f6ceb3bb70aba65
Fabian Nicollier <fabian.nicollier@symbioticsgroup.com>
2015-01-09 15:00:28 +0100

    Renamed AutoProtobuf adapter project.

---
+++ src/TagCache.Redis.AutoProtobuf/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TagCache.Redis.AutoProtobuf")]
+[assembly: AssemblyCompany("Fabian Nicollier")] 
+[assembly: AssemblyDescription("An adapter for the TagCache.Redis library's Protobuf serializer and Autoprotobuf.")]
+[assembly: AssemblyProduct("TagCache.Redis.AutoProtobuf")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ src/TagCache.Redis.AutoProtobuf/SerializerBuilder.cs
@@ -0,0 +1,12 @@
+﻿using TagCache.Redis.ProtoBuf;
+
+namespace TagCache.Redis.AutoProtobuf
+{
+    public class SerializerBuilder : ISerializerBuilder
+    {
+        public void Build<T>()
+        {
+            global::AutoProtobuf.SerializerBuilder.Build<T>(); 
+        }
+    }
+}
