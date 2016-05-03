372508b3070ec1191734b375649c91288adbfff6
Fabian Nicollier <fabian.nicollier@symbioticsgroup.com>
2016-05-03 16:37:58 +0200

    Updated to TagCache.Redis 1.0.0.2

--- src/TagCache.Redis.AutoProtobuf/Properties/AssemblyInfo.cs
+++ src/TagCache.Redis.AutoProtobuf/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@
 [assembly: AssemblyCompany("Fabian Nicollier")] 
 [assembly: AssemblyDescription("An adapter for the TagCache.Redis library's Protobuf serializer and Autoprotobuf.")]
 [assembly: AssemblyProduct("TagCache.Redis.AutoProtobuf")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
-[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyVersion("0.1.1.0")]
